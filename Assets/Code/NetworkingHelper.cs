8bc724aec5feaa567650710e1efa64ca0c8d6c79
Esmayl Mourad <esmayl@jellyfishdm.com>
2016-05-23 12:32:07 +0200

    Initial commit

---
+++ Assets/Code/NetworkingHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public class NetworkingHelper : MonoBehaviour
+{
+    const string checkedIn = "koffieAan";
+
+    string currentIp;
+    string url;
+
+    int networkIp = 1;
+    int subnetIp = 107;
+
+    HttpWebRequest webLink;
+
+    void Start()
+    {
+        currentIp = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+        url = String.Format("http://192.168.{0}.{1}", networkIp, subnetIp);
+    }
+
+    public void KoffieAan()
+    {
+        webLink = new HttpWebRequest(new Uri(url));
+        
+        webLink.UserAgent = "Domotica app " + currentIp;
+        webLink.Method = "POST";
+        webLink.ContentLength = checkedIn.Length;
+        webLink.ContentType = "application/x-www-form-urlencoded";
+
+        Stream webStream = webLink.GetRequestStream();
+
+        byte[] encoded = Encoding.UTF8.GetBytes(checkedIn);
+
+        webStream.Write(encoded,0,encoded.Length);
+
+        webStream.Close();
+
+        WebResponse response = webLink.GetResponse();
+
+        webStream = response.GetResponseStream();
+
+        StreamReader reader = new StreamReader(webStream);
+
+        string responseString = reader.ReadToEnd();
+
+        if (responseString != "")
+        {
+            reader.Close();
+            webStream.Close();
+            response.Close();
+
+            Debug.Log("KoffieAan response: "+responseString);
+
+        }
+
+    }
+
+
+}
---
+++ Assets/InputHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+
+	void Update ()
+    {
+	    if (Input.GetKeyUp(KeyCode.Escape))
+	    {
+	        Application.Quit();
+	    }
+	}
+}
